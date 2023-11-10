5850f154f0e557d8160636f4de634a3dbf127476
Eric <haunt3dd@gmail.com>
2023-11-10 11:56:34 +0700

    update

--- script/main.js
+++ script/main.js
@@ -228,7 +228,7 @@ const animationTimeline = () => {
       },
       "-=2"
     )
-    .to(
+    .from(
       ".jj2-dp",
       0.5,
       {
@@ -240,7 +240,7 @@ const animationTimeline = () => {
       },
       "-=2"
     )
-    .to(
+    .from(
       ".jj3-dp",
       0.5,
       {
@@ -252,7 +252,7 @@ const animationTimeline = () => {
       },
       "-=2"
     )
-    .to(
+    .from(
       ".jj4-dp",
       0.5,
       {
@@ -264,7 +264,7 @@ const animationTimeline = () => {
       },
       "-=2"
     )
-    .to(
+    .from(
       ".jj5-dp",
       0.5,
       {
